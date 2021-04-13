c046b57fe42d3f19afb888f4a5064e715849c487
tomekpg <tomekpg@op.pl>
2021-04-13 20:41:24 +0200

    Enemy part 1.

---
+++ Unity/HungryDoors/Assets/Code/EnemyController.cs
@@ -0,0 +1,55 @@
+﻿using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : Character
+{
+    [Header("Movement")]
+    public Transform movementGoal;
+    NavMeshAgent agent;
+
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        //if (agent.remainingDistance < 1f)
+        //{
+        //    StopAgentMovement();
+        //}
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag(Tags.PLAYER))
+        {
+            movementGoal = other.transform;
+            agent.destination = movementGoal.position;
+        }
+    }
+
+    [Button]
+    private void StopAgentMovement()
+    {
+        agent.isStopped = true;
+    }
+
+    [Button]
+    private void StartAgentMovement()
+    {
+        agent.isStopped = false;
+    }
+
+    public override void Die()
+    {
+        base.Die();
+        Debug.Log($"Enemy {gameObject.name} dead.");
+    }
+}
