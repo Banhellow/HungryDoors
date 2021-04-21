1a0f99bd6c78d758a8dfae7af6be1540ee900f7f
tomekpg <tomekpg@op.pl>
2021-04-21 18:20:21 +0200

    Menu part 1.

---
+++ Unity/HungryDoors/Assets/Code/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public Button lvl1Btn;
+    public Button lvl2Btn;
+    public Button lvl3Btn;
+
+    public void OnLevelClick(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void OnExitClick()
+    {
+        Application.Quit();
+    }
+}
