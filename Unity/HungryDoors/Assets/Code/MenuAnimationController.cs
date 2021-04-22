4c47cab5d74f69d89a2d445ee9700cc308bc736d
Wlad <wladyslaw@neurodio.com>
2021-04-22 17:15:06 +0200

    menu animation bind

---
+++ Unity/HungryDoors/Assets/Code/MenuAnimationController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAnimationController : MonoBehaviour
+{
+
+    public Animator chillAnim;
+    void Start()
+    {
+        chillAnim.SetTrigger("Chill");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
